110c82dd2b10ced55b3feb579b924400c8c53e3d
Stephen-Payne <Stephen-Payne@users.noreply.github.com>
2020-10-09 17:54:07 -0400

    Update main.js

--- public/lab_5/main.js
+++ public/lab_5/main.js
@@ -4,7 +4,8 @@ function testFunction() {
     .then((response) => {
          console.log(response);
     })
-    document.querySelector("title").innerHTML = response.text;
-    document.querySelector("h1").innerHTML = response.text;
+    document.querySelector("title").innerHTML = 'response.text';
+    document.querySelector("h1").innerHTML = 'response.text';
     document.body.style.backgroundColor = 'rgb(113, 2, 2)';
+    document.getElementsByClassName('flex-outer').style.margin = '0 auto'
 }
