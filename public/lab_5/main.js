d330e4cea58b94e53b85ae47780b54779a0c8627
Stephen-Payne <Stephen-Payne@users.noreply.github.com>
2020-10-09 20:02:03 -0400

    Update main.js

--- public/lab_5/main.js
+++ public/lab_5/main.js
@@ -7,11 +7,11 @@ function testFunction() {
     document.querySelector("title").innerHTML = 'response.text()';
     document.querySelector("h1").innerHTML = 'response.text()';
     document.body.style.backgroundColor = 'rgb(113, 2, 2)';
-    document.getElementsByClassName('flex-outer').style.margin = '0px';
+    //document.getElementsByClassName('flex-outer').style.margin = '0px';
     document.querySelector("p.checkbox-list-label").innerHTML = 'Fruits';
 
     var fruits = ["banana", "durian", "pineapple"];
-    document.querySelectorAll(".flex-inner li").forEach(fruitsFunction);
+    document.querySelectorAll('input[type="checkbox"]').label.forEach(fruitsFunction);
 
     function fruitsFunction(item, index) {
         item.innerHTML = fruits[index];
