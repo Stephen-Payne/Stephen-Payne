1eb3fc27bc5cfddcc59daed30de874307667fe97
Stephen-Payne <Stephen-Payne@users.noreply.github.com>
2020-10-30 22:00:21 -0400

    Update script.js

--- public/lab_7/script.js
+++ public/lab_7/script.js
@@ -17,48 +17,6 @@ function convertRestaurantsToCategories(restaurantList) {
 
 function makeYourOptionsObject(datapointsFromRestaurantsList) {
   // set your chart configuration here!
-  const chart = new CanvasJS.Chart("chartContainer", {
-    animationEnabled: true,
-    
-    title:{
-      text:"Places To Eat Out In Future"
-    },
-    axisX:{
-      interval: 1
-    },
-    axisY2:{
-      interlacedColor: "rgba(1,77,101,.2)",
-      gridColor: "rgba(1,77,101,.1)",
-      title: "Restaurants by Category"
-    },
-    data: [{
-      type: "bar",
-      name: "companies",
-      axisYType: "secondary",
-      color: "#014D65",
-      dataPoints: [
-        { y: 3, label: "Sweden" },
-        { y: 7, label: "Taiwan" },
-        { y: 5, label: "Russia" },
-        { y: 9, label: "Spain" },
-        { y: 7, label: "Brazil" },
-        { y: 7, label: "India" },
-        { y: 9, label: "Italy" },
-        { y: 8, label: "Australia" },
-        { y: 11, label: "Canada" },
-        { y: 15, label: "South Korea" },
-        { y: 12, label: "Netherlands" },
-        { y: 15, label: "Switzerland" },
-        { y: 25, label: "Britain" },
-        { y: 28, label: "Germany" },
-        { y: 29, label: "France" },
-        { y: 52, label: "Japan" },
-        { y: 103, label: "China" },
-        { y: 134, label: "US" }
-      ]
-    }]
-  });
-  chart.render();
   CanvasJS.addColorSet('customColorSet1', [
     // add an array of colors here https://canvasjs.com/docs/charts/chart-options/colorset/
   ]);
@@ -78,7 +36,20 @@ function makeYourOptionsObject(datapointsFromRestaurantsList) {
       gridColor: 'rgba(1,77,101,.1)',
       title: 'Restaurants by Category',
       labelFontSize: 12,
-      scaleBreaks: {customBreaks: []} // Add your scale breaks here https://canvasjs.com/docs/charts/chart-options/axisy/scale-breaks/custom-breaks/
+      scaleBreaks: {customBreaks: [
+        {
+          startValue: 40,
+          endValue: 50
+        },
+        {
+          startValue: 85,
+          endValue: 100
+        },
+        {
+          startValue: 140,
+          endValue: 175
+        }
+      ]}, // Add your scale breaks here https://canvasjs.com/docs/charts/chart-options/axisy/scale-breaks/custom-breaks/
     },
     data: [{
       type: 'bar',
