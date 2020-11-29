3c4c647a5a060a0878135f4f5bbb3140d7e5691d
Stephen-Payne <Stephen-Payne@users.noreply.github.com>
2020-11-28 20:52:52 -0500

    Update server.js

--- server.js
+++ server.js
@@ -40,6 +40,11 @@ async function databaseInitialize(dbSettings) {
  
 }
 
+async function foodDataFetcher() {
+  const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+  const json = await data.json();
+}
+
 
 dotenv.config();
 
@@ -79,8 +84,7 @@ app.route('/sql')
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
 
-    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
-    const json = await data.json();
+    
     console.log('data from fetch', json);
     res.json(json);
   });
